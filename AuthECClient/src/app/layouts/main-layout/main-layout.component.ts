608b55d91bf4877934b42ef1a169ccd52149b685
yoshua137 <p4810137@gmail.com>
2025-07-16 13:51:04 -0400

    addF: styles navbar hide menu layout static

--- AuthECClient/src/app/layouts/main-layout/main-layout.component.ts
+++ AuthECClient/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,24 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
 import { HideIfClaimsNotMetDirective } from '../../directives/hide-if-claims-not-met.directive';
 import { claimReq } from "../../shared/utils/claimReq-utils";
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive, HideIfClaimsNotMetDirective, CommonModule],
   templateUrl: './main-layout.component.html',
-  styles: ``
+  styles: ``,
+  animations: [
+    trigger('slideDownUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('300ms cubic-bezier(0.4,0,0.2,1)', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms cubic-bezier(0.4,0,0.2,1)', style({ transform: 'translateY(-100%)', opacity: 0 }))
+      ])
+    ]),
+    trigger('navSlide', [
+      transition(':enter', []), // no-op para evitar advertencias
+      transition('* => *', [
+        animate('300ms cubic-bezier(0.4,0,0.2,1)')
+      ])
+    ])
+  ]
 })
 export class MainLayoutComponent {
   showHeaderMenu = true;
+  showHeaderBar = true;
 
   constructor(private router: Router,
     private authService: AuthService) { }
 
   claimReq = claimReq
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showHeaderBar = window.scrollY === 0;
+  }
 
   onLogout() {
     this.authService.deleteToken();
